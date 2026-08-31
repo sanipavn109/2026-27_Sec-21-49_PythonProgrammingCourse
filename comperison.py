0ada7d575debb89004ff0884343fbe588261df93
sanipavn109 <sanipavan109@gmail.com>
2026-08-31 16:00:59 +0530

    comperison.py

---
+++ comperison.py
@@ -0,0 +1,14 @@
+a=3
+b=9
+result=a>b
+print("result of",a,">",b,"is :",result)
+result=a<b
+print("result of",a,"<",b,"is :", result)
+result=a==b
+print("result of",a==b,"is :",result)
+result=a!=b
+print("result  of",a!=b,"is :",result)
+result=a>=b
+print("result  of",a>=b,"is :",result)
+result=a<=b
+print("result  of",a<=b,"is :",result)
