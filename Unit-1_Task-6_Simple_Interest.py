5fbc9783c67930a6555c14932e389ef089aa8f53
sanipavn109 <sanipavan109@gmail.com>
2026-09-04 15:51:53 +0530

    simple interest

---
+++ Unit-1_Task-6_Simple_Interest.py
@@ -0,0 +1,6 @@
+# to calculate the simple interst
+principal=float(input("enter the principal amount:"))
+rate=float(input("enter the rate of interest:"))
+time=float(input("enter the time in months:"))
+interest=(principal*rate*time)/100
+print(interest)
