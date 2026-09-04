c657c5d34d86514cbd7cfaaaa40c27a06f0c36b0
sanipavn109 <sanipavan109@gmail.com>
2026-09-04 14:21:54 +0530

    logical operator

---
+++ logical operators.py
@@ -0,0 +1,8 @@
+#python logical operator exercise
+a=8
+result =a>2 and a<10
+print("result of",a,">2,and",a,"<10", result)
+result=a>2 and a<10
+print("result of",a,">9 and",a,"<5",result)
+result=a<9 and a>19
+print("result of",a,"<9 and",a,"<5", result)
